a2919f156583b320d1ea3ae4e86ebb38b478323e
Cine-Pixel <ikaumphriani@gmail.com>
2021-03-22 18:52:51 +0400

    Work 3

---
+++ basics/Work3/Perimeter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work3 {
+    // 7.4.2
+    class SumClass{
+        public virtual int Metodi1(int[] nums) {
+            return nums.Sum();
+        }
+    }
+    
+    class ProductClass : SumClass {
+        public override int Metodi1(int[] nums) {
+            int prod = 1;
+            foreach(int num in nums) {
+                prod *= num;
+            }
+            return prod;
+        }
+    }
+}
---
+++ basics/Work3/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work3 {
+    // 7.1.3
+    class RectangleBase {
+        private int baseSide;
+
+        public int BaseSide {
+            get { return baseSide; }
+        }
+
+        public void PrintBase() {
+            Console.WriteLine(baseSide);
+        }
+        public RectangleBase(int s) {
+            baseSide = s;
+        }
+    }
+    class Rectangle : RectangleBase {
+        private int height;
+
+        public int GetArea() {
+            return height * BaseSide;
+        }
+        public Rectangle(int h, int s) : base(s) { 
+            height = h;
+        }
+    }
+}
